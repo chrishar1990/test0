5500471bcb4a5fb33f7cd44fa13793fd48501020
Christopher Harris <ch185222@ncr.com>
2022-09-29 10:02:59 -0400

    updated list

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from github import Github
+import git
+import subprocess
+from datetime import datetime
+from git import Repo
+
+from git import Repo
+repo = Repo('/Users/CH185222/practice/test0/')
+repo.git.add('--all')
+repo.git.commit('-m', 'updated list')
+origin = repo.remote(name='test0')
+origin.push()
+
